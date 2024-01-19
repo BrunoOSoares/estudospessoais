8c9832f0ae3bfd41236156bcc72bd7954c827874
Bruno de Oliveira Soares <brunonobre146@gmail.com>
2024-01-19 20:20:55 -0300

    Voltando aos estudos e revendo logica

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que peça ao usuario para digirar um numero inteiro,
+informe se este numero é par ou impar. Caso o usuario não digite um numero
+inteiro, informe que não é um numero inteiro.
+
+"""
+
+"""
+Faça um programa que pergunte a hora ao usuario e, baseando-se no horario
+descrito, exiba a saudação apropriada. Ex.
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+
+"""
+
+"""
+Faça um programa que peça o primeiro nome do usuario. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande".
+
+"""
